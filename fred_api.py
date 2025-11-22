555aa5ef93e913873a573bdec6700c5c1a222b7f
Phu Le <phule@umich.edu>
2025-11-22 15:55:15 -0500

    Added fred_api.py and bug fixes and tests

---
+++ fred_api.py
@@ -0,0 +1,16 @@
+import requests
+from typing import List, Dict
+from datetime import datetime
+from config import FRED_API_KEY, FRED_BASE_URL
+from db import get_connection
+
+# FRED series IDs we care about
+FRED_SERIES = {
+    "effr": "FEDFUNDS",      # Effective Federal Funds Rate
+    "treasury_10y": "DGS10", # 10-year Treasury
+    "baa_yield": "BAA"       # Moody's BAA Corporate Bond Yield
+}
+
+
+
+
